6e91998b47fa168a1c3afe8ff7dda8c9fb31fd81
Jen <jennadockery2000@gmail.com>
2026-04-29 16:37:46 -0400

    Create temperatures.py

---
+++ temperatures.py
@@ -0,0 +1,22 @@
+def convert_temperature():
+    print("Enter which conversion you want:")
+    print("Select '1' Celsius to Fahrenheit")
+    print("Select '2' Fahrenheit to Celsius")
+    #Prompt user choice
+    choice = input("Enter choice '1' or '2': ")
+    
+    #Prompt to enter temperature value
+    temp = float(input("Enter temperature value: "))
+    
+    if choice == '1':
+        # (C * 9/5) + 32
+        result = (temp * 9/5) + 32
+        print(f"{temp}°C is equal to {result:.2f}°F")
+    elif choice == '2':
+        # (F - 32) * 5/9
+        result = (temp - 32) * 5/9
+        print(f"{temp}°F is equal to {result:.2f}°C")
+    else:
+        print("Invalid selection.")
+
+convert_temperature()
