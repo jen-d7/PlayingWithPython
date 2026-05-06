51fcd0b08757acac7492ac48e54a90a53468a000
Jen <jennadockery2000@gmail.com>
2026-05-06 12:05:57 -0400

    Update math.py

--- math.py
+++ math.py
@@ -1,3 +1,5 @@
+#Addition
+
 def sum(a, b):
     return (a + b)
 
@@ -5,3 +7,32 @@ a = int(input('Enter 1st number: '))
 b = int(input('Enter 2nd number: '))
 
 print(f'Sum of {a} and {b} is {sum(a, b)}')
+
+#Subtraction
+def subt(a, b):
+    return (a - b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Subtraction of {a} and {b} is {subt(a, b)}')
+
+#Multiplication
+
+def multi(a, b):
+    return (a * b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'Product of {a} and {b} is {multi(a, b)}')
+
+#Division
+def div(a, b):
+    return (a / b)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+print(f'The division of {a} and {b} is {div(a, b)}')
+
